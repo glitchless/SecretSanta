b3e12de5050fee1e16877ac0e86c7445fcd06760
LionZXY <nikita@kulikof.ru>
2021-12-06 18:31:53 +0300

    Fix tp name

--- backend/src/main/kotlin/ru/glitchless/santa/repository/auth/tp/TPAuthSource.kt
+++ backend/src/main/kotlin/ru/glitchless/santa/repository/auth/tp/TPAuthSource.kt
@@ -6,20 +6,23 @@ enum class TPAuthSource(host: String) {
     @SerializedName("technopark")
     TECHNOPARK("park.vk.company"),
 
-    @SerializedName("mailcourses")
-    MAILCOURSES("education.vk.company"),
-
-    @SerializedName("data")
-    DATA("data.vk.company"),
-
-    @SerializedName("polis")
-    POLIS("polis.vk.company"),
+    @SerializedName("sphere")
+    SPHERE("sphere.vk.company"),
 
     @SerializedName("track")
     TRACK("track.vk.company"),
 
-    @SerializedName("sphere")
-    SPHERE("sphere.vk.company"),
+    @SerializedName("atom")
+    ATOM("technoatom.vk.company"),
+
+    @SerializedName("polis")
+    POLIS("polis.vk.company"),
+
+    @SerializedName("data")
+    DATA("data.vk.company"),
+
+    @SerializedName("education")
+    EDUCATION("education.vk.company"),
 
     @SerializedName("gamesphere")
     GAMESPHERE("gamesphere.ru");
